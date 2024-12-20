131873ea6b10f48d138286d6c7db8bf47e145c4f
migueluzcategui <miguel.uzcategui@vortexa.com>
2024-12-20 09:51:20 +0000

    feat: improve second part of challenge 1 on day 1 2024

--- node/src/index.ts
+++ node/src/index.ts
@@ -1,4 +1,5 @@
 import { ChallengeConfig } from "./types";
+import chalk from 'chalk';
 import { day01Challenge } from "./year2024/day01";
 import path from "path";
 import { performance } from "perf_hooks";
@@ -34,6 +35,7 @@ const challenges: ChallengeConfig[] = [
 const start = (): void => {
   const { year, day } = extractYearAndDay();
 
+  // Filter the challenges based on the year and day provided
   const availableChallenges = challenges.filter((challenge) => {
     if (day !== undefined) {
       return challenge.year === year && challenge.day === day;
@@ -44,22 +46,56 @@ const start = (): void => {
     return true;
   });
 
-  const start = performance.now();
-  
-  availableChallenges.forEach((challenge) => {
+  // Read the input files for the challenges
+  const inputsMap: Map<string, string> = new Map();
+  availableChallenges.map((challenge) => {
     const pathName = `input/${challenge.year}/${challenge.day}.txt`;
     const filePath = path.resolve(__dirname, "../../", pathName);
     const fileContents = readFileSync(filePath, "utf-8");
+    inputsMap.set(`${challenge.year}/${challenge.day}`, fileContents);
+  });
+
+  // Execute the challenges
+  const start = performance.now();
+  availableChallenges.forEach((challenge) => {
+    const fileContents = inputsMap.get(`${challenge.year}/${challenge.day}`);
+    if (!fileContents) {
+      throw new Error("Input file not found");
+    }
     const parsedData = challenge.challenge.parse(fileContents);
+   
+    const part1Start = performance.now();
     const part1Result = challenge.challenge.part1(parsedData);
+    const part1End = performance.now();
+   
+    const part2Start = performance.now();
     const part2Result = challenge.challenge.part2(parsedData);
+    const part2End = performance.now();
+    
     console.log(
-      `Year: ${challenge.year}, Day: ${challenge.day}, Part 1: ${part1Result}, Part 2: ${part2Result}`
+      chalk.bold(`\n${chalk.greenBright(`Year: ${challenge.year}, Day: ${challenge.day}`)}`)
+    );
+    console.log(
+      chalk.blueBright(
+        `   Part 1: ${chalk.yellow(part1Result)}, ms: ${chalk.magenta((part1End - part1Start).toFixed(2))}`
+      )
+    );
+    console.log(
+      chalk.blueBright(
+        `   Part 2: ${chalk.yellow(part2Result)}, ms: ${chalk.magenta((part2End - part2Start).toFixed(2))}`
+      )
     );
   });
-  
+
   const end = performance.now();
-  console.log(`Execution time: ${(end - start).toFixed(2)} milliseconds`);
+
+  console.log(
+    chalk.bold(
+      chalk.red(
+        `\nExecution time: ${chalk.white((end - start).toFixed(2))} milliseconds\n`
+      )
+    )
+  );
 };
 
 start();
--- node/src/year2024/day01.ts
+++ node/src/year2024/day01.ts
@@ -6,15 +6,34 @@ export const day01Challenge: Challenge = {
   parse: parseStringToVectorOfIntegers,
   part1: (data: InputParsed) => {
     let distance = 0;
+    // Sort the vectors
     const firstVectorSorted = data.firstVector.sort((a, b) => a - b);
     const secondVectorSorted = data.secondVector.sort((a, b) => a - b);
 
+    // pair the vectors, and get the total distance between the pairs
     for (let i = 0; i < firstVectorSorted.length; i++) {
       distance += Math.abs(firstVectorSorted[i] - secondVectorSorted[i]);
     }
+
+    // return the total distance
     return distance;
   },
-  part2: () => {
-    return 0;
+  part2: (data: InputParsed) => {
+    let score = 0;
+    // Sort the vectors
+    const firstVectorSorted = data.firstVector.sort((a, b) => a - b);
+    const secondVectorSorted = data.secondVector.sort((a, b) => a - b);
+
+    firstVectorSorted.forEach((value) => {
+        let count = 0;
+        secondVectorSorted.forEach((value2) => {
+            if(value === value2) {
+                count++;
+            }
+        });
+        score += value * count;
+    });
+
+    return score;
   },
 };
